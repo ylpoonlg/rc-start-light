629e6b4eff8e8f7aee888f477eb6371da79eabb7
ylpoonlg <poon.yat.long@gmail.com>
2025-01-12 00:04:24 +0000

    Clean up config file

--- rc-start-light/config.h
+++ rc-start-light/config.h
@@ -1,18 +1,16 @@
-// Verbosity of the debug log
-//  -1: Disable logs.
-//   0: Enables virtual LED (Prints simulated strip to serial).
-//   >0: Enable debug logs.
-#define VERBOSE 0
-
 // Pin assignments
+//	 * Primary LED strip
 #define STRIP0_PIN 5
+//	 * Secondary LED strip (optional)
 #define STRIP1_PIN 6
-#define RST_BTN_PIN 2
+//	 * IR receiver
 #define IR_RCV_PIN 4
+//	 * Reset button (optional)
+#define RST_BTN_PIN 2
 
 // Number of pixels per led strips
 #define NUM_PIXELS 20
-// Number of steps in the countdown (preferably divisible from NUM_PIXELS)
+// Number of steps in the countdown
 #define NUM_STEPS 3
 // Which LED pattern to use
 //   0: Sequential
@@ -22,21 +20,29 @@
 // Reverse countdown pattern
 #define PATTERN_REVERSE true
 
+// IR receiver protocol
+#define IR_PROTO NEC
+// IR commands - Set VERBOSE to 4 (or above) and check serial monitor to find
+// the command for each button on the remote.
+//   * Reset
+#define IR_CMD_RST 0x40
+//   * Start
+#define IR_CMD_STR 0x41
+//   * Turn off
+#define IR_CMD_OFF 0x5D
+
 // Interval between each step (in milliseconds)
 #define COUNT_INTERVAL 1000
 // Interval between each refresh loop (in milliseconds)
 #define REFRESH_INTERVAL 50
 
-// IR receiver protocol
-#define IR_PROTO NEC
-// Command to use for reset
-#define IR_CMD_RST 0x40
-// Command to use for start
-#define IR_CMD_STR 0x41
-// Command to use for LED off
-#define IR_CMD_OFF 0x5D
-
 // Customize colors
 #define COLOR_RED   0xff0000
 #define COLOR_AMBER 0xffaa00
 #define COLOR_GREEN 0x00ff00
+
+// Verbosity of the debug log
+//  -1: Disable logs.
+//   0: Enables virtual LED (Prints simulated strip to serial).
+//   >0: Enable debug logs.
+#define VERBOSE 0
